8459fb6bcffa72a53e2f31e2e8f9a0d547c5057e
ltsinh <linhtsinh@gmail.com>
2018-07-05 21:21:43 -0400

    Fix Devise

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  extend Devise::Models
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         has_many :grams
-         has_many :comments
-end
